f243ec3ccfa32d27fdf1bd35fd6d9dcb0872ee02
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-09-07 15:47:43 +0200

    Add project files.

---
+++ Core_Console/Beaver.cs
@@ -0,0 +1,18 @@
+﻿using Shared;
+
+namespace ReadModel;
+
+public class BeaverCommand
+{
+	public void AddBeaverCommand(Beaver beaver)
+	{
+	}
+}
+public class BeaverQuery
+{
+	public Beaver GetBeaver(Beaver? beaver)
+	{
+		new BeaverCommand().AddBeaverCommand(beaver!);
+		return null;
+	}
+}
---
+++ Framework_Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared;
+
+namespace Framework_Console
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+
+
+	public class BeaverCommand
+	{
+		public void AddBeaverCommand(Beaver beaver)
+		{
+		}
+	}
+	public class BeaverQuery
+	{
+		public Beaver GetBeaver(Beaver beaver)
+		{
+			new BeaverCommand().AddBeaverCommand(beaver);
+			return null;
+		}
+	}
+}
