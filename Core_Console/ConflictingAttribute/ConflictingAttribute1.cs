043266de5150085da64d1d5732a5ae8acbdc3867
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2024-01-17 13:50:56 +0100

    Update Attributes.

---
+++ Core_Console/ConflictingAttribute/ConflictingAttribute1.cs
@@ -0,0 +1,37 @@
+﻿using JetBrains.Annotations;
+
+namespace Core_Console.ConflictingAttribute;
+
+/// <summary>
+///     [AttributeUsage(AttributeTargets.Method)]
+///     public class CommandMethodAttribute : Attribute { }
+///     public class CommandAttribute : Attribute { }
+///     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Struct)]
+///     public class CommandHandlerAttribute : Attribute { }
+///     [AttributeUsage(AttributeTargets.Method)]
+///     public class QueryMethodAttribute : Attribute { }
+///     public class QueryAttribute : Attribute { }
+///     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+///     public class QueryHandlerAttribute : Attribute { }
+///     public class NoCqrs : Attribute { }
+/// </summary>
+public class ConflictingAttribute1
+{
+    public class ClassWithUnidentifiedName
+    {
+        public void Test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class MyQuery
+    {
+        public void Test()
+        {
+            var a = new ClassWithUnidentifiedName();
+            a.Test();
+        }
+    }
+}
--- Multi/Attributes.cs
+++ Multi/Attributes.cs
@@ -1,20 +1,14 @@
 ﻿// ReSharper disable CheckNamespace
 namespace JetBrains.Annotations;
 
-[AttributeUsage(AttributeTargets.Method)]
-public class CommandMethodAttribute : Attribute { }
-
 public class CommandAttribute : Attribute { }
-
-[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Struct)]
-public class CommandHandlerAttribute : Attribute { }
-
-[AttributeUsage(AttributeTargets.Method)]
-public class QueryMethodAttribute : Attribute { }
 
 public class QueryAttribute : Attribute { }
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+public class CommandHandlerAttribute : Attribute { }
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
 public class QueryHandlerAttribute : Attribute { }
 
-public class NoCqrs: Attribute { }
+public class NoCqrsAttribute : Attribute { }
